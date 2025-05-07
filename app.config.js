d56ad511d008543d989dd7de43c08f44f512a85e
César José Delgado Suárez <cjdsgithub@gmail.com>
2025-05-07 17:41:30 +0100

    Se eliminó un comentario redundante relacionado con databaseURL y se añadieron los plugins necesarios de Firebase: auth, firestore, storage y database. Además, se comentó la inclusión de expo-router, ya que generalmente no es necesario en este archivo de configuración, mejorando así la claridad y adecuación del app.config.js.

--- app.config.js
+++ app.config.js
@@ -15,7 +15,7 @@ export default ({ config }) => ({
     firebaseConfig: {
       apiKey: process.env.FIREBASE_API_KEY,
       authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.FIREBASE_DATABASE_URL, // <-- ahora correcto
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
       projectId: process.env.FIREBASE_PROJECT_ID,
       storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
       messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
@@ -38,7 +38,11 @@ export default ({ config }) => ({
   },
   plugins: [
     '@react-native-firebase/app',
+    '@react-native-firebase/auth',
+    '@react-native-firebase/firestore', // <— Añadido Firestore
+    '@react-native-firebase/storage',
+    '@react-native-firebase/database', // Realtime Database
     ['expo-build-properties', { ios: { useFrameworks: 'static' } }],
-    'expo-router',
+    // 'expo-router', // no suele ser necesario aquí
   ],
 });
